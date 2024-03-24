fe7f7705676626651ae6d6134f22e2abfb633c62
acondor123 <chkreis400@gmail.com>
2024-03-23 20:41:36 -0800

    big website update
    Added error pages, proper amount of entry boxes, and more.

--- Code/website/app.py
+++ Code/website/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import qrcode
 import base64
 from io import BytesIO
+import re
 
 app = Flask(__name__)
 
@@ -12,11 +13,36 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
-        entry = ""
-        entry += request.form['firstName'] + ','
-        entry += request.form['lastName'] + ','
-        entry += request.form['phoneNumber'] + ','
-        qr_code_data = generate_qr_code(entry)  # Generate QR code with the submitted entry
+
+
+        firstName = request.form['firstName'].lower()
+        lastName = request.form['lastName'].lower()
+        rank = request.form['rank'].lower()
+        unit = re.sub('[^0-9]', '', request.form['unit'])
+        phoneNumber = re.sub('[^0-9]', '', request.form['phoneNumber'])
+
+
+        #following if statements check to see if the user submitted valid inputs
+        #If one of them is wrong, take them to the appropriate error page
+        if(firstName == ""):
+            return render_template('firstNameError.html')
+        
+        if(lastName == ""):
+            return render_template('lastNameError.html')
+
+        if(rank == ""):
+            return render_template('noRankError.html')
+
+        if(unit == ""):
+            return render_template('unitError.html')
+
+        if(len(phoneNumber) != 10):
+            return render_template('phoneNumberError.html', phoneNumberLength=len(phoneNumber))
+
+        #stores value of all entries in a single variable
+        entry = f"{firstName},{lastName},{rank},{unit},{phoneNumber}"
+
+        qr_code_data = generate_qr_code(entry)  # Generate QR code with values user enters
         return render_template('submitted.html', entry=entry, qr_code_data=qr_code_data)
 
 def generate_qr_code(data):
@@ -31,7 +57,7 @@ def generate_qr_code(data):
     qr.make(fit=True)
     # Create a BytesIO object to hold the QR code image
     qr_img = BytesIO()
-    img = qr.make_image(fill_color="black", back_color="white")
+    img = qr.make_image(fill_color='black', back_color='white')
     img.save(qr_img, format='PNG')
     # Encode the image as base64 string
     qr_img_base64 = base64.b64encode(qr_img.getvalue()).decode()
