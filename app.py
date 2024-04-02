dc81c1015c5fcf3db50a5a493a9b07ce45fce19f
acondor123 <chkreis400@gmail.com>
2024-04-01 20:48:42 -0800

    Updated app.py
    added additional return statements to validate functions to make it more clear

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def validateFirstName(name):
         return 'First Name Is Too Long.'
     if(bool(re.match(r'^[a-zA-Z-]+$', name.strip())) is False):
         return 'Invalid First Name.'
+    return False
 
 
 def validateLastName(name):
@@ -23,11 +24,13 @@ def validateLastName(name):
         return 'Last Name Is Too Long.'
     if(bool(re.match(r'^[a-zA-Z-]+$', name.strip())) is False):
         return 'Invalid Last Name.'
+    return False
 
 
 def validateRank(rank):
     if(not bool(rank)):
         return 'Please Choose A Rank From The Dropdown Menu.'
+    return False
 
 
 def validateUnit(unit):
@@ -35,6 +38,7 @@ def validateUnit(unit):
         return 'Please Enter A Unit.'
     if(len(unit) <= 2 or len(unit) >= 5):
         return 'Invalid Unit.'
+    return False
 
 
 def validatePhoneNumber(phoneNumber):
@@ -42,14 +46,17 @@ def validatePhoneNumber(phoneNumber):
         return 'Please Enter A Phone Number.'
     if(len(phoneNumber) != 10):
         return 'Invalid Length On The Phone Number.'
+    return False
 
 def validateFitness(fitness):
     if(fitness == 'none'):
         return 'Please Choose An Answer To The Fitness Question.'
+    return False
     
 def validateProfile(profile):
     if(profile == 'none'):
         return 'Please Choose An Answer To The Profile Question.'
+    return False
 
 
 @app.route('/')
@@ -70,8 +77,7 @@ def submit():
         profile = request.form['profile']
 
 
-        #following if statements check to see if the user submitted valid inputs
-        #If one of them is wrong, update the index.html page with appropriate error message
+        
         errorMessages = []
         errorMessages.append(validateFirstName(firstName))
         errorMessages.append(validateLastName(lastName))
@@ -81,7 +87,6 @@ def submit():
         errorMessages.append(validateFitness(fitness))
         errorMessages.append(validateProfile(profile))
         for message in errorMessages:
-            print(message)
             if(bool(message)):
                 return render_template('index.html', error=message)
 
