daf5098974b32daf793c4907873740ddba5840c9
acondor123 <82035569+acondor123@users.noreply.github.com>
2024-04-03 20:49:12 -0800

    Some suggested changes from code review

--- app.py
+++ app.py
@@ -6,50 +6,71 @@ import re
 
 app = Flask(__name__)
 
+maxNameLength = 64
+validPhoneNumberLength = 10
+minUnitLength = 2
+maxUnitLength = 5
+nameRegex = r'^[a-zA-ZÀ-ÖØ-öø-ÿ-]+$'
 
 def validateFirstName(name):
+
+
     if(not bool(name)):
        return 'Please Enter A First Name.'
-    if(len(name) >= 64):
+    elif(len(name) >= maxNameLength):
         return 'First Name Is Too Long.'
-    if(bool(re.match(r'^[a-zA-Z-]+$', name.strip())) is False):
+    elif(bool(re.match(nameRegex, name.strip())) is False):
         return 'Invalid First Name.'
+    else:
+        return False
 
 
 def validateLastName(name):
     if(not bool(name)):
        return 'Please Enter A Last Name.'
-    if(len(name) >= 64):
+    elif(len(name) >= maxNameLength):
         return 'Last Name Is Too Long.'
-    if(bool(re.match(r'^[a-zA-Z-]+$', name.strip())) is False):
+    elif(bool(re.match(nameRegex, name.strip())) is False):
         return 'Invalid Last Name.'
+    else:
+        return False
 
 
 def validateRank(rank):
     if(not bool(rank)):
         return 'Please Choose A Rank From The Dropdown Menu.'
+    else:
+        return False
 
 
 def validateUnit(unit):
     if(not bool(unit)):
         return 'Please Enter A Unit.'
-    if(len(unit) <= 2 or len(unit) >= 5):
+    elif(len(unit) <= minUnitLength or len(unit) >= maxUnitLength):
         return 'Invalid Unit.'
+    else:
+        return False
 
 
 def validatePhoneNumber(phoneNumber):
     if(not bool(phoneNumber)):
         return 'Please Enter A Phone Number.'
-    if(len(phoneNumber) != 10):
+    elif(len(phoneNumber) != validPhoneNumberLength):
         return 'Invalid Length On The Phone Number.'
+    else:
+        return False
 
 def validateFitness(fitness):
     if(fitness == 'none'):
         return 'Please Choose An Answer To The Fitness Question.'
+    else:
+        return False
     
 def validateProfile(profile):
     if(profile == 'none'):
         return 'Please Choose An Answer To The Profile Question.'
+    else:
+        return False
 
 
 @app.route('/')
@@ -81,7 +102,6 @@ def submit():
         errorMessages.append(validateFitness(fitness))
         errorMessages.append(validateProfile(profile))
         for message in errorMessages:
-            print(message)
             if(bool(message)):
                 return render_template('index.html', error=message)
 
