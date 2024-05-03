31b261be80a1d1aca895df043ea7e4509000e876
acondor123 <chkreis400@gmail.com>
2024-05-02 20:23:49 -0800

    Deliverables

---
+++ Final Deliverables/README.txt
@@ -0,0 +1,5 @@
+Contained in the Kiosk App folder you will find all files related to the kiosk interface
+
+Contained in the Web App folder you will find all files related to the website.
+
+Each folder contains its own user manual since they each seperate parts of the project with different uses.
---
+++ Final Deliverables/Web App/app.py
@@ -0,0 +1,179 @@
+from flask import Flask, render_template, request
+import qrcode
+import base64
+from io import BytesIO
+import re
+
+'''
+    Following file is responsible for implementing the website located at the following URL:
+        https://jberpme-96ea7d14ff9b.herokuapp.com/
+    All required installations are found in requirements.txt.
+    To deploy the app on the Heroku platform, requirements.txt and Procfile need to be present in the same folder as app.py.
+    The logo displayed on the screen is contained in the folder 'static' and it must remain in there.
+    The HTML files are located in the folder 'templates' and are required to display the website.
+    There should be two files in there: index.html AND submitted.html.
+
+    For app.py
+        Connor wrote    xx%
+        Mika wrote      xx%
+
+    For index.html
+        Connor wrote    xx%
+        Mika wrote      xx%
+
+    For sumbitted.html
+        Connor wrote    xx%
+        Mika wrote      xx%
+'''
+
+app = Flask(__name__)
+
+maxNameLength = 64
+validPhoneNumberLength = 10
+minUnitLength = 2
+maxUnitLength = 5
+nameRegex = r'^[a-zA-ZÀ-ÖØ-öø-ÿ-]+$'
+
+'''
+    All validate functions below ensure that the proper information is
+    being submitted by the user.  If a problem is encountered, a string
+    detailing the error is returned to the submit function.  This error
+    is then displayed for the user on the index.html page.
+'''
+def validateFirstName(name):
+
+    if(not bool(name)):
+       return 'Please Enter A First Name.'
+    elif(len(name) >= maxNameLength):
+        return 'First Name Is Too Long.'
+    elif(bool(re.match(nameRegex, name.strip())) is False):
+        return 'Invalid First Name.'
+    else:
+        return False
+
+
+def validateLastName(name):
+    if(not bool(name)):
+       return 'Please Enter A Last Name.'
+    elif(len(name) >= maxNameLength):
+        return 'Last Name Is Too Long.'
+    elif(bool(re.match(nameRegex, name.strip())) is False):
+        return 'Invalid Last Name.'
+    else:
+        return False
+
+
+def validateRank(rank):
+    validRanks = ["amn", "a1c", "sra", "ssgt", "tsgt", "msgt", "smsgt", "cmsgt"]
+    if(not bool(rank)):
+        return 'Please Choose A Rank From The Dropdown Menu.'
+    elif(rank not in validRanks):
+        return 'Please Enter A Valid Rank From The Dropdown Menu.'
+    else:
+        return False
+
+
+def validateUnit(unit):
+    if(not bool(unit)):
+        return 'Please Enter A Unit.'
+    elif(len(unit) <= minUnitLength or len(unit) >= maxUnitLength):
+        return 'Invalid Unit.'
+    else:
+        return False
+
+
+def validatePhoneNumber(phoneNumber):
+    if(not bool(phoneNumber)):
+        return 'Please Enter A Phone Number.'
+    elif(len(phoneNumber) != validPhoneNumberLength):
+        return 'Invalid Length On The Phone Number.'
+    else:
+        return False
+
+def validateFitness(fitness):
+    if(fitness == 'none'):
+        return 'Please Choose An Answer To The Fitness Question.'
+    else:
+        return False
+    
+def validateProfile(profile):
+    if(profile == 'none'):
+        return 'Please Choose An Answer To The Profile Question.'
+    else:
+        return False
+
+'''
+    Below function renders the home screen when website is first opened.
+'''
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+'''
+    Below function handles the logic taken when form is submitted.
+    It validates the input against desired format and then formats
+    the data appropriately.  It then calls generate_qr_code to
+    make the QR code the student will keep with them.
+'''
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+
+
+        firstName = request.form['firstName'].strip().lower()
+        lastName = request.form['lastName'].strip().lower()
+        rank = request.form['rank'].strip().lower()
+        unit = re.sub('[^0-9]', '', request.form['unit']).strip()
+        phoneNumber = re.sub('[^0-9]', '', request.form['phoneNumber']).strip()
+        fitness = request.form['fitness']
+        profile = request.form['profile']
+
+
+        #following checks for errors when the user submits.  Only the first message
+        #is printed in order to avoid overcrowding the screen.
+        errorMessages = []
+        errorMessages.append(validateFirstName(firstName))
+        errorMessages.append(validateLastName(lastName))
+        errorMessages.append(validateRank(rank))
+        errorMessages.append(validateUnit(unit))
+        errorMessages.append(validatePhoneNumber(phoneNumber))
+        errorMessages.append(validateFitness(fitness))
+        errorMessages.append(validateProfile(profile))
+        for message in errorMessages:
+            if(bool(message)):
+                return render_template('index.html', error=message)
+
+
+        #stores value of all entries in a single variable
+        entry = f"{firstName},{lastName},{rank},{unit},{phoneNumber},{fitness},{profile}$"
+
+        qr_code_data = generate_qr_code(entry)
+        return render_template('submitted.html', entry=entry, qr_code_data=qr_code_data)
+
+
+'''
+    Uses the library qrcode to generate QR codes form the information
+    gathered from the form the student fills out on the home page.
+    This function returns a BytesIO object that is then displayed
+    as specified by the HTML in submitted.html.
+'''
+def generate_qr_code(data):
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    # BytesIO makes it so qrcode doesn't need to be stored
+    qr_img = BytesIO()
+    img = qr.make_image(fill_color='black', back_color='white')
+    img.save(qr_img)
+    # Encode the image as base64 string
+    qr_img_base64 = base64.b64encode(qr_img.getvalue()).decode()
+    return qr_img_base64
+
+if __name__ == '__main__':
+    app.run()
